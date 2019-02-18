6b787c98ffb86eba88bcee456686dbcb8fbe7497
andrei3008 <andrei3008@gmail.com>
2019-02-18 11:34:12 +0200

    authentication back-end

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'Salut lume'
+        message: 'Salut ${req.body.email}! Te-ai inregistrat cu succes!'
     })
 })
 
